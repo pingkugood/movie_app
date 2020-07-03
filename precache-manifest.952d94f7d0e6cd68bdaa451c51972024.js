3987ed78459427f40f8624308607bc5dea5af575
One <rkddnjs999@gmail.com>
2020-07-03 19:15:29 +0900

    Updates

---
+++ precache-manifest.952d94f7d0e6cd68bdaa451c51972024.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07f4c3e6014a7a73eac0cb3ee996fbdd",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "78a8c6d4641937053d35",
+    "url": "/movie_app/static/js/2.19735fbb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app/static/js/2.19735fbb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "24915d3b9976483ac31f",
+    "url": "/movie_app/static/js/main.56d61a23.chunk.js"
+  },
+  {
+    "revision": "793037b19926e1970ea2",
+    "url": "/movie_app/static/js/runtime-main.8469f341.js"
+  }
+]);
